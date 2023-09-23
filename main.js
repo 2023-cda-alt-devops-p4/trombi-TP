c5a58ff1e887ea700af63c3bbf0166d295c93b15
Theo Pierson <thheopierson@gmail.com>
2023-09-23 10:51:25 +0200

    feat: place profiles on the map close to their city

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const positionsProfiles = [
-    { left: 20, top: 30, name: "Theo Pierson" }, 
-    { left: 40, top: 60, name: "Camille Lafrance" }, 
-    { left: 40, top: 50, name: "Diego Garcia" },
-    { left: 40, top: 30, name: "Alexandre Foucher" },
-    { left: 40, top: 40, name: "Jonathan Moreschi" },
-    { left: 40, top: 60, name: "Pierre Fraisse" },
-    { left: 30, top: 60, name: "Yassine El Azzati" },
-    { left: 40, top: 70, name: "Thomas Mortelette" },
-    { left: 40, top: 80, name: "Zakaria Laskari" },
-    { left: 20, top: 80, name: "Marine Beauvois" },
-    { left: 70, top: 80, name: "Audrey Carpentier" },
-    { left: 40, top: 90, name: "Nassim Deflandre" },
-    { left: 40, top: 95, name: "Mélanie Chauvin" },
-    { left: 10, top: 80, name: "Ludovic Phounphonh" }
+    { left: 34.5, top: 44, name: "Theo Pierson" }, 
+    { left: 38, top: 83, name: "Camille Lafrance" }, 
+    { left: 40, top: 80, name: "Diego Garcia" },
+    { left: 52.7, top: 58, name: "Alexandre Foucher" },
+    { left: 45, top: 85, name: "Jonathan Moreschi" },
+    { left: 39.5, top: 46, name: "Pierre Fraisse" },
+    { left: 34, top: 54, name: "Yassine El Azzati" },
+    { left: 40.1, top: 44, name: "Thomas Mortelette" },
+    { left: 42, top: 43.5, name: "Zakaria Laskari" },
+    { left: 41.2, top: 44, name: "Marine Beauvois" },
+    { left: 33, top: 73, name: "Audrey Carpentier" },
+    { left: 42.2, top: 46.2, name: "Nassim Deflandre" },
+    { left: 42, top: 45, name: "Mélanie Chauvin" },
+    { left: 41, top: 47.8, name: "Ludovic Phounphonh" }
 ];
 
 const pointContainer = document.querySelector('.map-container');
